2a3c9de809476363c4bf96c4b89dbec43dcd5b51
ugo-en <ugochukwuenwachukwu@gmail.com>
2022-08-20 23:04:50 +0100

    Final day!!!

---
+++ machine_learning_demo/main1.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+inputs = tf.keras.Input(shape=(3,))
+x = tf.keras.layers.Dense(4, activation=tf.nn.relu)(inputs)
+outputs = tf.keras.layers.Dense(5, activation=tf.nn.softmax)(x)
+model = tf.keras.Model(inputs=inputs, outputs=outputs)
+print(model)
